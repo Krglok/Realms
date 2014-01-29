f8a1f29aea05a59f6c770ae3a8cb234951aebf9f
Krglok <duda.olaf@drachen-horst.de>
2014-01-29 15:56:17 +0100

    New manager bodys

---
+++ RealmModel/src/net/krglok/realms/manager/CraftManager.java
@@ -0,0 +1,18 @@
+package net.krglok.realms.manager;
+
+/**
+ * the Craft Manager realize the Controller and manager for the production of the settlement.
+ * 
+ * @author oduda
+ *
+ */
+public class CraftManager
+{
+
+	public CraftManager()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
